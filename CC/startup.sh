6c6b49d3c5e219fdff22ef8405ad726f302d4725
arulpm018 <72654003+arulpm018@users.noreply.github.com>
2024-12-13 14:22:27 +0700

    Update startup.sh

--- CC/startup.sh
+++ CC/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt update && sudo apt install tmux
 sudo mkdir -p model
 sudo mkdir -p vector_store
 sudo gsutil -m cp -r gs://ipbgptbucket/model/* model/
@@ -7,8 +8,9 @@ sudo gsutil -m cp -r gs://ipbgptbucket/vector_store/* vector_store/
 
 sudo git clone https://github.com/arulpm018/ipbgptserver.git
 pip install -r requirements.txt
-export HF_KEY=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
+export HF_KEY=<HF-KEY>
 
 cd ipbgptserver
 
+tmux new -s mysession
 exec uvicorn main:app --host 0.0.0.0 --port 8000
